69464e99cb119b097462dbe8136b2b3e8a2f0725
Elena-Martinez-cloud <elena.martinez-elejarza@g.austincc.edu>
2025-02-25 05:55:54 +0000

    homework 3

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,61 @@
-//write include statement for decisions header
+//write include statement for decisions heade
+#include "decisions.h"
 
-
+using std::string;
 //Write code for function(s) code here
+
+string get_letter_grade_using_if(int grade)
+{
+    string letterGrade = "";
+
+
+    if (grade >= 90 && grade <= 100)
+    {
+        letterGrade = "A";
+    }
+    else if (grade >= 80 && grade <= 89) 
+    {
+        letterGrade = "B";
+    }
+    else if (grade >= 70 && grade <= 79) 
+    {
+        letterGrade = "C";
+    }
+    else if (grade >= 60 && grade <= 69)
+    {
+        letterGrade = "D";
+    }
+    else 
+    {
+        letterGrade = "F";
+    }
+    return letterGrade;
+}
+
+string get_letter_grade_using_switch(int grade){
+    int theletterGrade = grade / 10;
+    string letterGrade = "";
+
+    switch (theletterGrade)
+    {
+    case 10:
+    case 9:
+        letterGrade = "A";
+        break;
+    case 8:
+        letterGrade = "B";
+        break;
+    case 7:
+        letterGrade = "C";
+        break;
+    
+    case 6:
+        letterGrade = "D";
+        break;
+
+    default:
+        letterGrade = "F";
+        break;
+    }
+    return letterGrade;
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,45 @@
 //write include statements
+#include "decisions.h"
+#include<iostream>
+
+using std::cout;
+using std::cin;
 
 int main() 
 {
+
+	int choice;
+	int grade;
+
+	cout << "Main Menu" << "\n";
+	cout << "1 - Letter grade using if" <<"\n";
+	cout << "2 - Letter grade using switch" <<"\n";
+	cout << "3 - Exit" <<"\n";
+	cout << "Enter your choice: " <<"\n";
+	cin >> choice;
+
+	switch (choice)
+	{
+	case 1:
+		cout << "Enter numerical grade: ";
+		cin >> grade;
+		cout << "Letter grade using if: "<< get_letter_grade_using_if(grade) <<"\n";
+		break;
+
+	case 2:
+		cout << "Enter numerical grade: ";
+		cin >> grade;
+		cout << "Letter grade using switch: "<< get_letter_grade_using_switch(grade) <<"\n";
+		break;
+	
+	case 3:
+		cout << "Exiting the program";
+		break;
+	
+	default:
+		cout << "Invalid choice. Exiting the program.";
+		break;
+	}
+
 	return 0;
 }
