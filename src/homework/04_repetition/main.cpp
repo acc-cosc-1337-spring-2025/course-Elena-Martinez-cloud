8426d1f20847f7c2c347eb281b1dc1cfc7c9bc19
Elena-Martinez-cloud <elena.martinez-elejarza@g.austincc.edu>
2025-03-02 04:57:03 +0000

    homework 4

--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,8 +1,57 @@
 //write include statements
 
+#include <iostream>
+#include "repetition.h"
+
+using std::cout;
+using std::cin;
 //write using statements
 
 int main() 
 {
+	int choice;
+	bool exitprogram = false;
+
+	cout << "Menu:" << "\n";
+	cout << "1 - Factorial" << "\n";
+	cout << "2 - Greatest Common Divisor (GCD)" << "\n";
+	cout << "3 - Exit" << "\n";
+	cout << "Enter your choice: ";
+	cin >> choice;
+
+	switch (choice){
+		case 1: {
+			int num;
+			cout << "Enter a number to calculate factorial: ";
+			cin >> num;
+			cout << "Factorial of " << num << " is " << factorial(num);
+			break;
+		}
+
+		case 2: {
+			int num1, num2;
+			cout << "Enter two numbers to calculate GCD: ";
+			cin >> num1 >> num2;
+			cout << "GCD of " << num1 << " and " << num2 << " is " << gcd(num1, num2);
+			break;
+		}
+
+		case 3: {
+			char confirm;
+			cout << "Would You Like To Exit? (Y/N): ";
+			cin >> confirm;
+			if (confirm == 'Y'){
+				exitprogram = true;
+			}else{
+				cout << "Return to main menu";
+			}
+			break;
+
+		}
+	} while (!exitprogram);
+	
+
+	cout << "Exiting program";
+
 	return 0;
 }
--- src/homework/04_repetition/repetition.cpp
+++ src/homework/04_repetition/repetition.cpp
@@ -1,3 +1,24 @@
 //add include statements
+#include "repetition.h"
 
 //add function(s) code here
+
+int factorial(int num) {
+    int product = 1;
+    while (num > 0){
+        product *= num;
+        num--;
+    }
+    return product;
+}
+
+int gcd(int num1, int num2){
+    while (num1 != num2){
+        if (num1 < num2){
+            num2 -= num1;
+        } else{
+            num1 -= num2;
+        }
+    }
+    return num1;
+}
