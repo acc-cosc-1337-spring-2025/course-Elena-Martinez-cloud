339601500eb73bb869c163094d81ea39c4221ce4
Elena-Martinez-cloud <elena.martinez-elejarza@g.austincc.edu>
2025-03-09 05:54:00 +0000

    homework 5- functions

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,45 @@
 //add include statements
 
+#include <iostream>
+#include <string>
+#include "func.h"
+using std::string;
+
 //add function code here
+
+double get_gc_content(const string& dna) {
+    int gc_count = 0;
+    for (char base : dna) {
+        if (base == 'G' || base =='C'){
+            gc_count++;
+        }
+    }
+    return static_cast<double>(gc_count) / dna.length();
+}
+
+string reverse_string(string dna){
+    string reversed;
+    for (int i = dna.length() - 1; i >= 0; i--){
+        reversed += dna[i];
+    }
+    return reversed;
+}
+
+string get_dna_complement(string dna){
+    string reversed_dna = reverse_string(dna);
+
+    for (char& base : reversed_dna){
+        if (base == 'A'){
+            base ='T';
+        } else if (base == 'T'){
+            base = 'A';
+        } else if (base == 'C'){
+            base = 'G';
+        } else if (base == 'G'){
+            base = 'C';
+        } else {
+            return "Error: Invalid character in DNA sequence";
+        }
+    }
+    return reversed_dna;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,44 @@
 
+#include <iostream>
+#include "func.h"
+
+using std::cout;
+using std::cin;
+
+
 int main() 
 {
+	int choice;
+	string dna;
 
+	while (true){
+		cout << "Menu:" <<"\n";
+		cout << "1 - Get GC Content" << "\n";
+		cout << "3 - Exit" << "\n";
+
+		cout << "Enter your choice (1,2,3): ";
+		cin >> choice;
+
+		if (choice == 1){
+			cout << "Enter a DNA string: ";
+			cin >> dna;
+			double gcContent = get_gc_content(dna);
+			cout << "GC Content: " << gcContent<<"\n";
+		}
+		else if (choice == 2){
+			cout << "Enter a DNA String: ";
+			cin >> dna;
+			string complement = get_dna_complement(dna);
+			cout << "DNA complement: " << complement <<"\n";
+		}
+		else if (choice == 3){
+			cout << "Exiting the Program"<<"\n";
+			break;
+		}
+		else{
+			cout << "ERROR! Please try again."<<"\n";
+		}
+	}
+    
 	return 0;
 }
